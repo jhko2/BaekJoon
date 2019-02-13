5da0e934693c44e2ff0f967b5844f507bd5ffc6d
jhko2 <jhko2@naver.com>
2019-02-13 17:02:32 +0900

    Update Fibo.c

--- Fibo.c
+++ Fibo.c
@@ -10,6 +10,8 @@ unsigned long long MulMat(unsigned long long n)//Matrix a / 회수 n
 	unsigned long long R_Arr[2][2] = { { 1,1 },{ 1,0 } };
 	if ((n == 1) || (n == 2))
 		return 1;
+	else if (n == 0)
+		return 0;
 	else
 	{
 		if (n % 2 == 0) //짝수
@@ -17,29 +19,34 @@ unsigned long long MulMat(unsigned long long n)//Matrix a / 회수 n
 			T_Arr[0][0] = MulMat(n / 2); //분할
 			T_Arr[0][1] = MulMat(n / 2 - 1);
 			T_Arr[1][0] = MulMat(n / 2 - 1);
-			T_Arr[0][0] = MulMat(n / 2 - 2);
+			T_Arr[1][1] = MulMat(n / 2 - 2);
 
-			R_Arr[0][0] = (T_Arr[0][0] * T_Arr[1][0]) + (T_Arr[1][0] * T_Arr[0][1]); //정복
+			R_Arr[0][0] = (T_Arr[0][0] * T_Arr[0][0]) + (T_Arr[1][0] * T_Arr[0][1]); //정복
 			R_Arr[0][1] = T_Arr[0][0] * T_Arr[0][1] + T_Arr[0][1] * T_Arr[1][1];
 			R_Arr[1][0] = R_Arr[0][1];
 			R_Arr[1][1] = T_Arr[1][0] * T_Arr[0][1] + T_Arr[1][1] * T_Arr[1][1];
 		}
 		else //홀수
 		{
-			T_Arr[0][0] = MulMat(n / 2); //분할
-			T_Arr[0][1] = MulMat(n / 2 - 1);
-			T_Arr[1][0] = MulMat(n / 2 - 1);
-			T_Arr[0][0] = MulMat(n / 2 - 2);
+			if (n == 3)
+				return 2;
+			else
+			{
+				T_Arr[0][0] = MulMat(n / 2); //분할
+				T_Arr[0][1] = MulMat(n / 2 - 1);
+				T_Arr[1][0] = MulMat(n / 2 - 1);
+				T_Arr[1][1] = MulMat(n / 2 - 2);
 
-			R_Arr[0][0] = (T_Arr[0][0] * T_Arr[1][0]) + (T_Arr[1][0] * T_Arr[0][1]); //정복
-			R_Arr[0][1] = T_Arr[0][0] * T_Arr[0][1] + T_Arr[0][1] * T_Arr[1][1];
-			R_Arr[1][0] = R_Arr[0][1];
-			R_Arr[1][1] = T_Arr[1][0] * T_Arr[0][1] + T_Arr[1][1] * T_Arr[1][1];
+				R_Arr[0][0] = (T_Arr[0][0] * T_Arr[0][0]) + (T_Arr[1][0] * T_Arr[0][1]); //정복
+				R_Arr[0][1] = T_Arr[0][0] * T_Arr[0][1] + T_Arr[0][1] * T_Arr[1][1];
+				R_Arr[1][0] = R_Arr[0][1];
+				R_Arr[1][1] = T_Arr[1][0] * T_Arr[0][1] + T_Arr[1][1] * T_Arr[1][1];
 
-			R_Arr[0][0] = (R_Arr[0][0] * Arr[1][0]) + (R_Arr[1][0] * Arr[0][1]); //정복--> 홀수는 뒤에 Arr을 한번 더 곱해준다
-			R_Arr[0][1] = R_Arr[0][0] * Arr[0][1] + R_Arr[0][1] * Arr[1][1];
-			R_Arr[1][0] = R_Arr[0][1];
-			R_Arr[1][1] = R_Arr[1][0] * Arr[0][1] + R_Arr[1][1] * Arr[1][1];
+				R_Arr[0][0] = (R_Arr[0][0] * Arr[1][0]) + (R_Arr[1][0] * Arr[0][1]); //정복--> 홀수는 뒤에 Arr을 한번 더 곱해준다
+				R_Arr[0][1] = R_Arr[0][0] * Arr[0][1] + R_Arr[0][1] * Arr[1][1];
+				R_Arr[1][0] = R_Arr[0][1];
+				R_Arr[1][1] = R_Arr[1][0] * Arr[0][1] + R_Arr[1][1] * Arr[1][1];
+			}
 		}
 		return R_Arr[0][0];
 	}
